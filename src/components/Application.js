ed1b81dd08323312ca7004b495d6510e0d114016
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-10-27 16:44:20 -0600

    Add axios as dependency + useEffect into App.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import DayList from 'components/DayList'
 import Appointment from 'components/Appointment/index';
 
@@ -59,26 +61,27 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  const [day, setDay] = useState(["Monday"]);
   const appointmentList = appointments.map((appointment) => { return <Appointment key={appointment.id} {...appointment} /> })
 
   return (
