cd775ac3fe93f9d89460cc6a5289a314fbbdf1a3
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-10-27 17:52:00 -0600

    Add useEffect to App component to call days API

--- src/components/Application.js
+++ src/components/Application.js
@@ -61,28 +61,20 @@ const appointments = [
   }
 ];
 
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
-
 export default function Application(props) {
   const [days, setDays] = useState([]);
   const [day, setDay] = useState(["Monday"]);
   const appointmentList = appointments.map((appointment) => { return <Appointment key={appointment.id} {...appointment} /> })
+
+  useEffect(() => {
+    const daysAPI = 'http://localhost:8001/api/days';
+    axios.get(daysAPI)
+      .then(response => {
+        console.log('response :', response);
+        setDays([...response.data]);
+      })
+
+  }, [])
 
   return (
     <main className="layout">
