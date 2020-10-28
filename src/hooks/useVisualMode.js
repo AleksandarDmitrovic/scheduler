7ff655753a28b71253917bb68508fd7988600cde
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-10-28 14:26:53 -0600

    Add transition + back fns to useVisualMode Hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,8 +2,36 @@ import { useState } from "react";
 
 const useVisualMode = function (initialMode) {
   const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
 
-  return { mode };
+  const transition = function (newMode) {
+    setMode(() => newMode); //history.pop()
+    history.push(newMode);
+
+    setHistory(history)
+    // console.log('history :', history);
+
+  };
+  const back = function () {
+    console.log("HERE")
+    history.pop()
+
+    const historyLength = history.length;
+
+
+
+    setMode(history[historyLength - 1])
+
+
+    setHistory(history)
+
+  }
+
+  return {
+    mode,
+    transition,
+    back
+  };
 }
 
 export default useVisualMode;
