5ab7b482fd9f090d531137da2e79abdef5191ebf
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-10-28 20:17:47 -0600

    Add put request to API for new appts booked

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,6 +47,11 @@ export default function Application(props) {
       ...state.appointments,
       [id]: appointment
     };
+    axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
+      .then(res => {
+        console.log('res :', res);
+
+      })
 
     setState({
       ...state,
