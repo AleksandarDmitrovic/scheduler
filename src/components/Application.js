e21392e869780a5a4032b5dd38219928f577a0de
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-10-28 20:02:01 -0600

    Implement Interview booking update

--- src/components/Application.js
+++ src/components/Application.js
@@ -39,7 +39,19 @@ export default function Application(props) {
   }, [])
 
   const bookInterview = (id, interview) => {
-    console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    setState({
+      ...state,
+      appointments
+    });
   }
 
   const appointments = getAppointmentsForDay(state, state.day);
