08856639ec83d0d954dd2b27756bbf2ebc30e0df
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-10-30 19:18:10 -0600

    WebSocket refactor -regression test required

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,9 +1,3 @@
-// BELOW IS THE CODE BEFORE AND AFTER THE WEBSOCKET REFACTOR
-// I use to call useReducer inside of the book/deleteInterview functions with dispatch({ type: SET_INTERVIEW, id, interview }) 
-// but now I only call it withing the webSocket listener onMessage when there is a response signaling a database change
-// I also move the logic inside the setSpotsRemaining function into the reducer function case SET_INTERVIEW
-
-
 import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
@@ -25,18 +19,19 @@ function reducer(state, action) {
     case SET_INTERVIEW: {
       const id = action.id;
       const interview = action.interview ? { ...action.interview } : null;
-      console.log('interview :', interview);
+
       const appointment = {
         ...state.appointments[id],
         interview
       };
-      console.log('appointment :', appointment);
+
       const appointments = {
         ...state.appointments,
         [id]: appointment
       };
+
+      //Calculating Spots Reamining on UI side
       let numOfSpotsRemaining;
-      /////////////////////////////////////////////////
       let days = [
         ...state.days
       ]
@@ -52,9 +47,8 @@ function reducer(state, action) {
       }
 
       theSpotsDay.spots = numOfSpotsRemaining
-
       days[parseInt(theSpotsDay.id, 10) - 1] = theSpotsDay;
-      ////////////////////////////////////////////////////
+
       return {
         ...state,
         appointments,
@@ -78,56 +72,26 @@ const useApplicationData = () => {
 
   const setDay = (day) => dispatch({ type: SET_DAY, day });
 
-  // const setSpotsRemaining = (id, interview) => {
-  //   let numOfSpotsRemaining;
-  //   let days = [
-  //     ...state.days
-  //   ]
-
-  //   const [theSpotsDay] = days.filter(day => {
-  //     return day.appointments.includes(id)
-  //   })
-  //   console.log('theSpotsDay :', theSpotsDay);
-
-  //   if (interview === null) {
-  //     numOfSpotsRemaining = theSpotsDay.spots + 1
-  //   } else {
-  //     numOfSpotsRemaining = theSpotsDay.spots - 1
-  //   }
-
-  //   theSpotsDay.spots = numOfSpotsRemaining
-
-  //   days[parseInt(theSpotsDay.id, 10) - 1] = theSpotsDay;
-
-  //   return days
-  // }
-
-
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ]).then(all => {
-      const dataArr = all.map((res) => res.data)
-      const [days, appointments, interviewers] = dataArr
-      console.log("CALLED WEBSOCKET DISPATCH")
+      const dataArr = all.map((res) => res.data);
+      const [days, appointments, interviewers] = dataArr;
+
       dispatch({ type: SET_APPLICATION_DATA, days, appointments, interviewers });
     })
 
   }, [])
 
 
-
-
   const bookInterview = (id, interview) => {
-    // const days = setSpotsRemaining(id, interview)
 
     return axios.put(`/api/appointments/${id}`, { interview })
       .then(res => {
         // console.log('res :', res);
-        console.log("CALLED BOOKINTERVIEW")
-        // dispatch({ type: SET_INTERVIEW, id, interview }); //, days
       })
   }
 
@@ -138,8 +102,6 @@ const useApplicationData = () => {
     return axios.delete(`/api/appointments/${id}`, { data: { interview } })
       .then(res => {
         // console.log('res :', res);
-        console.log("CALLED DELETEINTERVIEW")
-        // dispatch({ type: SET_INTERVIEW, id, interview }); //, days
       })
   }
 
@@ -153,7 +115,6 @@ const useApplicationData = () => {
     }
     webSocket.onmessage = event => {
       const message = JSON.parse(event.data);
-      console.log('message :', message);
 
       if (message.type === "SET_INTERVIEW") {
         const id = message.id
@@ -161,9 +122,8 @@ const useApplicationData = () => {
         dispatch({ type: SET_INTERVIEW, id, interview })
       }
     }
-    //Cleanup needed
-    // webSocket.close();
-
+    //Cleanup 
+    return () => webSocket.close();
 
   }, []) //state.appointments
 
