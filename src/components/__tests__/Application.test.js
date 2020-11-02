3ff57632b12eaff3ca11143dbee158ed14601217
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-02 15:30:06 -0700

    Refactor Application test using async await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,13 +6,13 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText, toBeInTheDocument } = render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday"))
-    .then(() => {
-      fireEvent.click(getByText("Tuesday"));
+  await waitForElement(() => getByText("Monday"))
 
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    });
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+
 });
