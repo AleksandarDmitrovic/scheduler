d9bd4c23f3d46395e68db0a23e6bdebd7a4dab99
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-03 10:01:59 -0700

    Move reducer fn & constants to reducers folder

---
+++ src/reducers/application.js
@@ -0,0 +1,63 @@
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_DAY:
+      return { ...state, day: action.day }
+    case SET_APPLICATION_DATA:
+      return {
+        ...state,
+        days: action.days,
+        appointments: action.appointments,
+        interviewers: action.interviewers
+      }
+    case SET_INTERVIEW: {
+      const id = action.id;
+      const interview = action.interview ? { ...action.interview } : null;
+
+      const appointment = {
+        ...state.appointments[id],
+        interview
+      };
+
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment
+      };
+
+      //Calculating Spots Remaining on UI side
+
+      //loop through interviews arr and count which interview is null
+      const countSpotsOneDay = (dayObj, state) => {
+        let answer = 0;
+        for (let apptId of dayObj.appointments) {
+          if (appointments[apptId].interview === null) {
+            answer++;
+          }
+        }
+        return answer;
+      };
+
+      const days = state.days.map(day => {
+        let newSpotCount = countSpotsOneDay(day, state);
+        return { ...day, spots: newSpotCount };
+      });
+
+
+      return {
+        ...state,
+        appointments,
+        days
+      }
+    }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+export default reducer;
+export { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW };
