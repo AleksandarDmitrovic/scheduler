fe810190dccd5de4ac3261263e734c677180a9f8
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-02 18:21:57 -0700

    Initialize test plan in application.test.js

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -51,5 +51,20 @@ describe("Application", () => {
 
   });
 
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Click the "Delete" button on the first filled appointment. ----Click the "Delete" button on the booked appointment.
+    // 4. Check that the element with the text "Are you sure you would like to delete" is displayed. ---Check that the confirmation message is shown.
+    // 5. Click the "Confirm" button on that same appointment. --  Click the "Confirm" button on the confirmation.
+    // 6. Check that the element with the text "Deleting" is displayed.
+    // 7. Wait until the element with the Add button is displayed.
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2" spots remaining.
+  });
+
 });
 
